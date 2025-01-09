57c647ddb8fbac389cd285abf9ddefc6aa8ded9d
Sage Hourihan <samiho97@gmail.com>
2025-01-09 14:58:31 -0500

    getting movie poster

--- public/index.php
+++ public/index.php
@@ -1,11 +1,23 @@
 <?php 
-include_once '../config/db.php';
+include_once '../src/goodboycinema_helper.php';
 include_once '../views/header.php';
+
+$movies = getMovies();
 ?>
 
 <body>
     <h2>List of movies</h2>
+    <ul>
+        <?php
+            foreach ($movies as $movie) {
+                $id = getMovieID($movie['title']);
 
+                $poster = getMoviePoster($id);
+                // error_log($poster);
+                echo "<li><img src='$poster'></li>";
+            }
+        ?>
+    </ul>
 </body>
 
 <?php include_once '../views/footer.php' ?>
---
+++ src/goodboycinema_helper.php
@@ -0,0 +1,73 @@
+<?php
+include_once '/var/www/html/GoodBoyCinema/config/db.php';
+
+function getMovies(){
+    global $conn;
+    $stmt = $conn->prepare("SELECT * FROM movies");
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+function getMovieID($movie){
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://api.themoviedb.org/3/search/movie?query=' . urlencode($movie),
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_HTTPHEADER => array(
+        'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YjczZGVjOTNhMWZiMzI3ZWM1ZmE1ZDZlMGUwYjdkZSIsIm5iZiI6MTY1MzM1MTY5My45NTI5OTk4LCJzdWIiOiI2MjhjMjUwZDZjODQ5MjcxODdhZGIzYTAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.CaRtDmxuNNYnCRqmpk0D0daxRmP0sCTxod5dWSgUKDw'
+    ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    
+    $searchData = json_decode($response, true);
+
+    return $searchData['results'][0]['id'];
+}
+
+function getMoviePoster($id){
+
+    error_log($id);
+    $curl = curl_init();
+
+    curl_setopt_array($curl, [
+    CURLOPT_URL => "https://api.themoviedb.org/3/movie/$id/images?include_image_language=en",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => [
+        "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YjczZGVjOTNhMWZiMzI3ZWM1ZmE1ZDZlMGUwYjdkZSIsIm5iZiI6MTY1MzM1MTY5My45NTI5OTk4LCJzdWIiOiI2MjhjMjUwZDZjODQ5MjcxODdhZGIzYTAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.CaRtDmxuNNYnCRqmpk0D0daxRmP0sCTxod5dWSgUKDw",
+        "accept: application/json"
+    ],
+    ]);
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+
+    $response = json_decode($response, true);
+    $imagePath = $response['posters'][0]['file_path'];
+
+    $image = "https://image.tmdb.org/t/p/w200$imagePath";
+
+    if ($err) {
+    echo "cURL Error #:" . $err;
+    } else {
+    return $image;
+    }
+}
