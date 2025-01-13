8535829e17a499f3191f04fb70701e9e8b4377ed
Sage Hourihan <samiho97@gmail.com>
2025-01-13 14:34:09 -0500

    getting details of movie

--- src/request_goodboycinema.php
+++ src/request_goodboycinema.php
@@ -14,5 +14,30 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 }
 
 function getMovieDetails($id){
-    return "Movie id is $id";
+    // TODO: get movie desc from tmdb and actors
+    // return "movie id {$id}";
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, [
+    CURLOPT_URL => "https://api.themoviedb.org/3/movie/526007?language=en-US",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => [
+        "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmY0N2Y3NmFjM2U0NjFkMGJkZDNkNmQwZTRhYmJjMSIsIm5iZiI6MTY1MzM1MTY5My45NTI5OTk4LCJzdWIiOiI2MjhjMjUwZDZjODQ5MjcxODdhZGIzYTAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ykHUrFI7vdvSsy1qjseLXZgLRy2Rn4HRxS2yTf2gVLs",
+        "accept: application/json"
+    ],
+    ]);
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    return $response;
+
 }
