fc7fdc2bb7ba47cba3a0449dd6db3f6fb6842214
Sage Hourihan <samiho97@gmail.com>
2025-01-13 17:47:42 -0500

    removed ajax

--- public/details.php
+++ public/details.php
@@ -4,6 +4,29 @@ include_once '../views/header.php';
 
 $id = $_GET['id'];
 
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+CURLOPT_URL => "https://api.themoviedb.org/3/movie/526007?language=en-US",
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_ENCODING => "",
+CURLOPT_MAXREDIRS => 10,
+CURLOPT_TIMEOUT => 30,
+CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+CURLOPT_CUSTOMREQUEST => "GET",
+CURLOPT_HTTPHEADER => [
+    "Authorization: Bearer " . $_ENV['TOKEN'],
+    "accept: application/json"
+],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+$response = json_decode($response, true);
+
 ?>
 
 <main>
