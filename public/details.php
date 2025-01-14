2c9c83080bfb243ca381dcd53fd7f276d6fdaaa4
Sage Hourihan <samiho97@gmail.com>
2025-01-14 17:37:31 -0500

    getting details of specific movie

--- public/details.php
+++ public/details.php
@@ -1,43 +1,21 @@
 <?php 
 include_once '../src/goodboycinema_helper.php';
 include_once '../views/header.php';
+global $conn;
 
 $id = $_GET['id'];
 
-$curl = curl_init();
-
-curl_setopt_array($curl, [
-CURLOPT_URL => "https://api.themoviedb.org/3/movie/526007?language=en-US",
-CURLOPT_RETURNTRANSFER => true,
-CURLOPT_ENCODING => "",
-CURLOPT_MAXREDIRS => 10,
-CURLOPT_TIMEOUT => 30,
-CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-CURLOPT_CUSTOMREQUEST => "GET",
-CURLOPT_HTTPHEADER => [
-    "Authorization: Bearer " . $_ENV['TOKEN'],
-    "accept: application/json"
-],
-]);
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-$response = json_decode($response, true);
+$stmt = $conn->prepare("SELECT * FROM movies WHERE tmdb_id = :tmdb_id");
+$stmt->bindValue(":tmdb_id", $id);
+$stmt->execute();
+$results = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
 ?>
 
 <main>
 
-    <div class="details-container">
-        <div class="movie-card">
-            
-        </div>
 
-    </div>
 
 </main>
 
